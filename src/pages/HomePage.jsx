274cdcfdbf46d5623a33917e9550f2e5af2a66bf
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-05-19 15:11:18 -0700

    CATEGORIES added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from 'react'
+import Categories from '../components/home/Categories';
 import FeaturedProducts from '../components/home/FeaturedProducts'
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        
           <FeaturedProducts />
+          <Categories/>
       </Fragment>
     )
   }
 }
 
-export default HomePage
+export default HomePage;
