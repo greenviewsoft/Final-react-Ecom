165665dd681a66cf6e73089d3a11d3c226f762a7
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-07-02 02:29:24 +0600

    bug issue solved for router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,12 +6,9 @@ import '../src/assets/css/custom.css';
 import '../src/assets/css/fontawesome.css';
 import '../src/assets/css/animate.min.css';
 import '../src/assets/css/placeholder-loading.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
+import { render } from "react-dom";
 
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
+render(<App />, root);
 
 reportWebVitals();
